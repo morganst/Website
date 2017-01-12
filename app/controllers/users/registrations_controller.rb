3f67f154a0f00c7e76b7f195b7a7c79820d67594
Morgan Sturtz <msturtz@my.apsu.edu>
2017-01-12 19:30:55 +0000

    Added server side code for creating subscriptions for pro users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def create 
+  def create
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
         if resource.plan_id == 2
           resource.save_with_subscription
-        else 
+        else
           resource.save
         end
       end
